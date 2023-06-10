615e3ce9a42409666f1af0468b496ad28ab86f76
Bipin Khanal <bipin.khanal1050@gmail.com>
2023-06-10 22:13:59 +0545

    initial setup

---
+++ src/components/Headers.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Headers = () => {
+  return (
+    <div className='h-16 flex flex-col m-auto'>
+        Happy Reading !!
+    </div>
+  )
+}
+
+export default Headers
